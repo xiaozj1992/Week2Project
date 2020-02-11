be7a33b2cf3bb6a7f50cd1a6e362d786004a90c6
Johnny Jiang <xiaozj1992@yahoo.com>
2020-02-12 01:03:13 +0800

    Animation V0.01
    Heading Animation Complete,
    Turn Animation Pending*2
    Whirlpool Animation Pending,
    Reef Animation Pending
    Marshes Animation Pending

---
+++ Week2Project_The2/Assets/AnimationTest.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationTest : MonoBehaviour
+{
+    private Animator[] animators;
+    private Animator manAni;
+    private Animator leftPadAni;
+    private Animator rightPadAni;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animators = GetComponentsInChildren<Animator>();
+        manAni = animators[0];
+        leftPadAni = animators[1];
+        rightPadAni = animators[2];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("q")) {
+            manAni.Play("ManRow_L");
+            leftPadAni.Play("LeftPad_T");
+        }
+        if (Input.GetKey("w")) {
+            manAni.Play("ManRow_H");
+            leftPadAni.Play("LeftPad_H");
+            rightPadAni.Play("RightPad_H");
+        }
+        if (Input.GetKeyDown("e")) {
+            manAni.Play("ManRow_R");
+        }
+    }
+}
